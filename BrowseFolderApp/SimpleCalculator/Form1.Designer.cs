0883dcf9ed3342e5f695ca4d0b7a5560c3d32d4c
vaishag24 <85676491+vaishag24@users.noreply.github.com>
2023-06-19 10:44:37 +0530

    Quick space

---
+++ BrowseFolderApp/SimpleCalculator/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace SimpleCalculator
+{
+    partial class FrmSimpleCalculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.txtSideB = new System.Windows.Forms.TextBox();
+            this.txtSideA = new System.Windows.Forms.TextBox();
+            this.lblSideA = new System.Windows.Forms.Label();
+            this.lblSideB = new System.Windows.Forms.Label();
+            this.lblArea = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.BackColor = System.Drawing.SystemColors.Highlight;
+            this.btnBrowse.Location = new System.Drawing.Point(418, 54);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(100, 48);
+            this.btnBrowse.TabIndex = 0;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = false;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(196, 274);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(100, 26);
+            this.txtResult.TabIndex = 1;
+            // 
+            // txtSideB
+            // 
+            this.txtSideB.Location = new System.Drawing.Point(196, 108);
+            this.txtSideB.Name = "txtSideB";
+            this.txtSideB.Size = new System.Drawing.Size(169, 26);
+            this.txtSideB.TabIndex = 3;
+            // 
+            // txtSideA
+            // 
+            this.txtSideA.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.txtSideA.ForeColor = System.Drawing.Color.Black;
+            this.txtSideA.Location = new System.Drawing.Point(196, 54);
+            this.txtSideA.Name = "txtSideA";
+            this.txtSideA.Size = new System.Drawing.Size(169, 26);
+            this.txtSideA.TabIndex = 4;
+            // 
+            // lblSideA
+            // 
+            this.lblSideA.AutoSize = true;
+            this.lblSideA.Location = new System.Drawing.Point(98, 54);
+            this.lblSideA.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblSideA.Name = "lblSideA";
+            this.lblSideA.Size = new System.Drawing.Size(52, 20);
+            this.lblSideA.TabIndex = 6;
+            this.lblSideA.Text = "SideA";
+            // 
+            // lblSideB
+            // 
+            this.lblSideB.AutoSize = true;
+            this.lblSideB.Location = new System.Drawing.Point(98, 118);
+            this.lblSideB.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblSideB.Name = "lblSideB";
+            this.lblSideB.Size = new System.Drawing.Size(52, 20);
+            this.lblSideB.TabIndex = 7;
+            this.lblSideB.Text = "SideB";
+            this.lblSideB.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // lblArea
+            // 
+            this.lblArea.AutoSize = true;
+            this.lblArea.Location = new System.Drawing.Point(98, 278);
+            this.lblArea.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblArea.Name = "lblArea";
+            this.lblArea.Size = new System.Drawing.Size(43, 20);
+            this.lblArea.TabIndex = 8;
+            this.lblArea.Text = "Area";
+            // 
+            // FrmSimpleCalculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 449);
+            this.Controls.Add(this.lblArea);
+            this.Controls.Add(this.lblSideB);
+            this.Controls.Add(this.lblSideA);
+            this.Controls.Add(this.txtSideA);
+            this.Controls.Add(this.txtSideB);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.btnBrowse);
+            this.Name = "FrmSimpleCalculator";
+            this.Text = "SimpleCalculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.TextBox txtSideB;
+        private System.Windows.Forms.TextBox txtSideA;
+        private System.Windows.Forms.Label lblSideA;
+        private System.Windows.Forms.Label lblSideB;
+        private System.Windows.Forms.Label lblArea;
+    }
+}
+
---
+++ BrowseFolderApp/SimpleCalculator/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;using System.Collections.Generic;
+using System.IO;using System.Security.Cryptography.X509Certificates;
+using System.Windows.Forms;namespace SimpleCalculator{    public partial class FrmSimpleCalculator : Form    {        public FrmSimpleCalculator()        {            InitializeComponent();        }        private void Form1_Load(object sender, EventArgs e)        {        }                public void btnBrowse_Click(object sender, EventArgs e)//Button to browse to the input file        {                                         OpenFileDialog open = new OpenFileDialog();//funtion for the open prompt            try            {                                if (open.ShowDialog() == DialogResult.OK )//When the user chooses the file and clicks open the file is opened                {                   StreamReader read = new StreamReader(File.OpenRead(open.FileName));//function to open the browsed file and read all the contents 
+
+                    DirectoryInfo directory=new DirectoryInfo(open.FileName);//returns the full path of the file + filename +.txt
+                    string filePath = Convert.ToString(directory);//converts info to string
+
+                    filePath=filePath.Remove(filePath.LastIndexOf('\\'));//removes the filename and extension after //
+
+                    
+
+                    StreamWriter write = new StreamWriter(filePath+"\\"+"result.txt");//function to create a txt file in path where file exits and write the results. 
+                
+                    var lineCount = File.ReadAllLines(open.FileName).Length;//funtion to count the total lines of inputs                   
+
+                    for (int i=0 ; i<lineCount ; i++)//for loop to read the inputs as well as write in the txt file
+                 
+                    {
+                        string input = read.ReadLine();// reads the ith lines of the txt file
+                        string[] sides=input.Split(',');//splits the string based on "," and stores in array as 2 inputs
+
+                       double sideA= double.Parse(sides[0]);
+                       double sideB= double.Parse(sides[1]);
+
+                        txtSideA.Text = sideA.ToString();
+                        txtSideB.Text = sideB.ToString();
+
+                        //calculates area using 2 sides and writes in the file
+                        string result=Convert.ToString(calcArea(sideA, sideB));
+                        txtResult.Text = result;    
+
+                        write.WriteLine(result);
+                       
+
+                    }                        read.Close();//close the input txt file                        read.Dispose();                        write.Close();//close the ouput file                }            }            catch (Exception ex)             {                Console.WriteLine(ex.Message);            }
+
+        }
+        
+        //Function to calculate right angle triangle using 2 sides as parameters
+        public double calcArea(double sideA,double sideB)
+        {
+            double area = 0;
+            area= 0.5*sideA*sideB;
+            return area;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }}
